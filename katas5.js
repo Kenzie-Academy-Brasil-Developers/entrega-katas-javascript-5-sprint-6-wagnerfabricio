1c1dd56477df378d8a51f005040c33160ab0bafa
Vagner Fabricio <wagnerfabricio@hotmail.com>
2021-10-13 17:29:13 -0300

    create functions reverseString and testReverseString

--- katas5.js
+++ katas5.js
@@ -1,1 +1,20 @@
 // implemente aqui as funções de teste
+
+const reverseString = (str) => {
+    return str.toLowerCase().split("").reverse().join("");
+}
+
+const testReverseString1 = () => {
+    let result = reverseString("vagner")
+    let expected = "rengav"
+    console.assert(result === expected, {
+        "funcao": "reverseString",
+        "esperado": expected,
+        "obteve": result
+    })
+}
+
+testReverseString1()
+const testReverseString2 = () => {
+
+}
